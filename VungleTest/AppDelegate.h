d2c31a471d2a08dfeb9e02c6aa96811a28000eb2
lixutao <lixt@nuanxin-health.com>
2019-05-29 14:08:42 +0800

    Initial Commit

---
+++ VungleTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VungleTest
+//
+//  Created by lixutao on 2019/5/29.
+//  Copyright © 2019年 lixutao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VungleTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  VungleTest
+//
+//  Created by lixutao on 2019/5/29.
+//  Copyright © 2019年 lixutao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
